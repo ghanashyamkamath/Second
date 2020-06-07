916ae7dc0fc84f86d1f0c33d71212edd7b675b61
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-06-07 20:39:24 +0530

    Before Studing Git

---
+++ app.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+
+// when we click burger it adds a classlist nav-open to nav
+
+burger.addEventListener("click", () =>{ 
+    
+    
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+    
+});
+
+links.forEach(link =>{
+
+    link.addEventListener('click', () =>{
+        nav.classList.toggle("nav-open");
+        burger.classList.toggle("toggle");
+    })
+})
